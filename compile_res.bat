51154fddcef8ea6fb175711d44a936bbc459d961
tugalsan karabacak <tugalsan@gmail.com>
2022-09-07 16:00:16 +0300

    jdk and netbeans update

--- compile_res.bat
+++ compile_res.bat
@@ -1,22 +1,22 @@
 @ECHO OFF
 echo compile_res...
 
-set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.2.8-hotspot
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-18.0.2.101-hotspot
 
 cd C:\me\codes\com.tugalsan\res\res-common
-cmd /c "C:\Program Files\NetBeans-13\netbeans\java\maven\bin\mvn.cmd" -Dfile.encoding=UTF-8 clean install -DskipTests -q
+cmd /c "C:\me\codes\ide\netbeans\java\maven\bin\mvn.cmd" clean install -DskipTests -q
 echo res 1/4
 
 cd C:\me\codes\com.tugalsan\res\res-other
-cmd /c "C:\Program Files\NetBeans-13\netbeans\java\maven\bin\mvn.cmd" -Dfile.encoding=UTF-8 clean install -DskipTests -q
+cmd /c "C:\me\codes\ide\netbeans\java\maven\bin\mvn.cmd" clean install -DskipTests -q
 echo res 2/4
 
 cd C:\me\codes\com.tugalsan\res\res-three4g
-cmd /c "C:\Program Files\NetBeans-13\netbeans\java\maven\bin\mvn.cmd" -Dfile.encoding=UTF-8 clean install -DskipTests -q
+cmd /c "C:\me\codes\ide\netbeans\java\maven\bin\mvn.cmd" clean install -DskipTests -q
 echo res 3/4
 
 cd C:\me\codes\com.tugalsan\res\res-threejs
-cmd /c "C:\Program Files\NetBeans-13\netbeans\java\maven\bin\mvn.cmd" -Dfile.encoding=UTF-8 clean install -DskipTests -q
+cmd /c "C:\me\codes\ide\netbeans\java\maven\bin\mvn.cmd" clean install -DskipTests -q
 echo res 4/4
 
 cd C:\me\codes\com.tugalsan\com.tugalsan.bat
