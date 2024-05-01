3ce6f2ede166d86d034938f6d8076cd1cff88b8e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-05-01 19:29:14 +0300

    .

--- load_app.bat
+++ load_app.bat
@@ -4,64 +4,62 @@ d:
 cd D:\bin\tomcat\home\webapps
 
 goto lbl%~1
-:: 7z d D:\bin\tomcat\home\webapps\app-backup.war -im!*.java -r
 
 :lbl0
 echo lbl0
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.backup\com.tugalsan.app.backup-server\target\war\app-backup.war D:\bin\tomcat\home\webapps\app-backup.war -exclude=*.java
-7z d -tzip D:\bin\tomcat\home\webapps\app-backup.war *.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.backup\com.tugalsan.app.backup-server\target\war\app-backup.war D:\bin\tomcat\home\webapps\app-backup.war
 
 :lbl1
 echo lbl1
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.calender\com.tugalsan.app.calender-server\target\war\app-calender.war D:\bin\tomcat\home\webapps\app-calender.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.calender\com.tugalsan.app.calender-server\target\war\app-calender.war D:\bin\tomcat\home\webapps\app-calender.war
 
 :lbl2
 echo lbl2
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.cloud\com.tugalsan.app.cloud-server\target\war\app-cloud.war D:\bin\tomcat\home\webapps\app-cloud.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.cloud\com.tugalsan.app.cloud-server\target\war\app-cloud.war D:\bin\tomcat\home\webapps\app-cloud.war
 
 :lbl3
 echo lbl3
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.color\com.tugalsan.app.color-server\target\war\app-color.war D:\bin\tomcat\home\webapps\app-color.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.color\com.tugalsan.app.color-server\target\war\app-color.war D:\bin\tomcat\home\webapps\app-color.war
 
 :lbl4
 echo lbl4
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.contact\com.tugalsan.app.contact-server\target\war\app-contact.war D:\bin\tomcat\home\webapps\app-contact.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.contact\com.tugalsan.app.contact-server\target\war\app-contact.war D:\bin\tomcat\home\webapps\app-contact.war
 
 :lbl5
 echo lbl5
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.database\com.tugalsan.app.database-server\target\war\app-database.war D:\bin\tomcat\home\webapps\app-database.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.database\com.tugalsan.app.database-server\target\war\app-database.war D:\bin\tomcat\home\webapps\app-database.war
 
 :lbl6
 echo lbl6
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.download\com.tugalsan.app.download-server\target\war\app-download.war D:\bin\tomcat\home\webapps\app-download.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.download\com.tugalsan.app.download-server\target\war\app-download.war D:\bin\tomcat\home\webapps\app-download.war
 
 :lbl7
 echo lbl7
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.file\com.tugalsan.app.file-server\target\war\app-file.war D:\bin\tomcat\home\webapps\app-file.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.file\com.tugalsan.app.file-server\target\war\app-file.war D:\bin\tomcat\home\webapps\app-file.war
 
 :lbl8
 echo lbl8
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.login\com.tugalsan.app.login-server\target\war\app-login.war D:\bin\tomcat\home\webapps\app-login.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.login\com.tugalsan.app.login-server\target\war\app-login.war D:\bin\tomcat\home\webapps\app-login.war
 
 :lbl9
 echo lbl9
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.network\com.tugalsan.app.network-server\target\war\app-network.war D:\bin\tomcat\home\webapps\app-network.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.network\com.tugalsan.app.network-server\target\war\app-network.war D:\bin\tomcat\home\webapps\app-network.war
 
 :lbl10
 echo lbl10
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.showcase2d\com.tugalsan.app.showcase2d-server\target\war\app-showcase2d.war D:\bin\tomcat\home\webapps\app-showcase2d.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.showcase2d\com.tugalsan.app.showcase2d-server\target\war\app-showcase2d.war D:\bin\tomcat\home\webapps\app-showcase2d.war
 
 :lbl11
 echo lbl11
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.showcase3d\com.tugalsan.app.showcase3d-server\target\war\app-showcase3d.war D:\bin\tomcat\home\webapps\app-showcase3d.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.showcase3d\com.tugalsan.app.showcase3d-server\target\war\app-showcase3d.war D:\bin\tomcat\home\webapps\app-showcase3d.war
 
 :lbl12
 echo lbl12
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.showcasejs\com.tugalsan.app.showcasejs-server\target\war\app-showcasejs.war D:\bin\tomcat\home\webapps\app-showcasejs.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.showcasejs\com.tugalsan.app.showcasejs-server\target\war\app-showcasejs.war D:\bin\tomcat\home\webapps\app-showcasejs.war
 
 :lbl13
 echo lbl13
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\app\com.tugalsan.app.table\com.tugalsan.app.table-server\target\war\app-table.war D:\bin\tomcat\home\webapps\app-table.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\app\com.tugalsan.app.table\com.tugalsan.app.table-server\target\war\app-table.war D:\bin\tomcat\home\webapps\app-table.war
 
 :lblEnd
 echo lblEnd
--- load_spi.bat
+++ load_spi.bat
@@ -8,71 +8,71 @@ goto lbl%~1
 
 :lbl0
 echo lbl0
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.acsrf\target\war\spi-acsrf.war D:\bin\tomcat\home\webapps\spi-acsrf.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.acsrf\target\war\spi-acsrf.war D:\bin\tomcat\home\webapps\spi-acsrf.war
 
 :lbl1
 echo lbl1
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.allow\target\war\spi-allow.war D:\bin\tomcat\home\webapps\spi-allow.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.allow\target\war\spi-allow.war D:\bin\tomcat\home\webapps\spi-allow.war
 
 :lbl2
 echo lbl2
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.backup\target\war\spi-backup.war D:\bin\tomcat\home\webapps\spi-backup.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.backup\target\war\spi-backup.war D:\bin\tomcat\home\webapps\spi-backup.war
 
 :lbl3
 echo lbl3
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.calender\target\war\spi-calender.war D:\bin\tomcat\home\webapps\spi-calender.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.calender\target\war\spi-calender.war D:\bin\tomcat\home\webapps\spi-calender.war
 
 :lbl4
 echo lbl4
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.color\target\war\spi-color.war D:\bin\tomcat\home\webapps\spi-color.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.color\target\war\spi-color.war D:\bin\tomcat\home\webapps\spi-color.war
 
 :lbl5
 echo lbl5
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.contact\target\war\spi-contact.war D:\bin\tomcat\home\webapps\spi-contact.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.contact\target\war\spi-contact.war D:\bin\tomcat\home\webapps\spi-contact.war
 
 :lbl6
 echo lbl6
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.database\target\war\spi-database.war D:\bin\tomcat\home\webapps\spi-database.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.database\target\war\spi-database.war D:\bin\tomcat\home\webapps\spi-database.war
 
 :lbl7
 echo lbl7
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.domain\target\war\spi-domain.war D:\bin\tomcat\home\webapps\spi-domain.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.domain\target\war\spi-domain.war D:\bin\tomcat\home\webapps\spi-domain.war
 
 :lbl8
 echo lbl8
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.file\target\war\spi-file.war D:\bin\tomcat\home\webapps\spi-file.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.file\target\war\spi-file.war D:\bin\tomcat\home\webapps\spi-file.war
 
 :lbl9
 echo lbl9
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.jsp\target\war\spi-jsp.war D:\bin\tomcat\home\webapps\spi-jsp.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.jsp\target\war\spi-jsp.war D:\bin\tomcat\home\webapps\spi-jsp.war
 
 :lbl10
 echo lbl10
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.license\target\war\spi-license.war D:\bin\tomcat\home\webapps\spi-license.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.license\target\war\spi-license.war D:\bin\tomcat\home\webapps\spi-license.war
 
 :lbl11
 echo lbl11
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.login\target\war\spi-login.war D:\bin\tomcat\home\webapps\spi-login.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.login\target\war\spi-login.war D:\bin\tomcat\home\webapps\spi-login.war
 
 :lbl12
 echo lbl12
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.network\target\war\spi-network.war D:\bin\tomcat\home\webapps\spi-network.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.network\target\war\spi-network.war D:\bin\tomcat\home\webapps\spi-network.war
 
 :lbl13
 echo lbl13
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.report\target\war\spi-report.war D:\bin\tomcat\home\webapps\spi-report.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.report\target\war\spi-report.war D:\bin\tomcat\home\webapps\spi-report.war
 
 :lbl14
 echo lbl14
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.showcase\target\war\spi-showcase.war D:\bin\tomcat\home\webapps\spi-showcase.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.showcase\target\war\spi-showcase.war D:\bin\tomcat\home\webapps\spi-showcase.war
 
 :lbl15
 echo lbl15
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.table\target\war\spi-table.war D:\bin\tomcat\home\webapps\spi-table.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.table\target\war\spi-table.war D:\bin\tomcat\home\webapps\spi-table.war
 
 :lbl16
 echo lbl16
-java -jar D:\bin\tomcat\home\lib\jakartaee-migration-1.0.8-shaded.jar D:\git\spi\com.tugalsan.spi.tomcat\target\war\spi-tomcat.war D:\bin\tomcat\home\webapps\spi-tomcat.war -exclude=*.java
+java -jar D:\git\api\com.tugalsan.api.migration.jakarta\target\jakartaee-migration-1.0.9-SNAPSHOT-shaded.jar D:\git\spi\com.tugalsan.spi.tomcat\target\war\spi-tomcat.war D:\bin\tomcat\home\webapps\spi-tomcat.war
 
 :lblEnd
 echo lblEnd
